716ee38e8779ef55bae6e301968c324078a2df62
evilieswithin <evilieswithin>
2023-12-27 09:28:28 +0530

    custom post type events

--- front-page.php
+++ front-page.php
@@ -15,34 +15,55 @@
         <div class="full-width-split__inner">
             <h2 class="headline headline--small-plus t-center">Upcoming Events</h2>
 
-            <div class="event-summary">
-                <a class="event-summary__date t-center" href="#">
-                    <span class="event-summary__month">Mar</span>
-                    <span class="event-summary__day">25</span>
-                </a>
-                <div class="event-summary__content">
-                    <h5 class="event-summary__title headline headline--tiny"><a href="#">Poetry in the 100</a></h5>
-                    <p>Bring poems you&rsquo;ve wrote to the 100 building this Tuesday for an open mic and snacks. <a href="#" class="nu gray">Learn more</a></p>
-                </div>
-            </div>
-            <div class="event-summary">
-                <a class="event-summary__date t-center" href="#">
-                    <span class="event-summary__month">Apr</span>
-                    <span class="event-summary__day">02</span>
-                </a>
-                <div class="event-summary__content">
-                    <h5 class="event-summary__title headline headline--tiny"><a href="#">Quad Picnic Party</a></h5>
-                    <p>Live music, a taco truck and more can found in our third annual quad picnic day. <a href="#" class="nu gray">Learn more</a></p>
-                </div>
-            </div>
+            <?php
+            $today = date('Ymd');
+            $homepageEvents = new WP_Query(array(
+                'posts_per_page' => 2,
+                'post_type' => 'event',
+                'meta-key' => 'event_date',
+                'orderBy' => 'meta_value_num',
+                'order' => 'ASC',
+                'meta_query' => array(
+                    array(
+                        'key' => 'event_date',
+                        'compare' => '>=',
+                        'value' =>  $today,
+                        'type' => 'numeric'
+                    )
+                )
+            ));
 
-            <p class="t-center no-margin"><a href="#" class="btn btn--blue">View All Events</a></p>
+            while ($homepageEvents->have_posts()) {
+                $homepageEvents->the_post(); ?>
+                <div class="event-summary">
+                    <a class="event-summary__date t-center" href="#">
+                        <span class="event-summary__month"><?php
+                                                            $eventDate = new DateTime(get_field('event_date'));
+                                                            echo $eventDate->format('M')
+                                                            ?></span>
+                        <span class="event-summary__day"><?php
+                                                            echo $eventDate->format('d')
+                                                            ?></span>
+                    </a>
+                    <div class="event-summary__content">
+                        <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
+                        <p><?php if (has_excerpt()) {
+                                echo  get_the_excerpt();
+                            } else {
+                                echo wp_trim_words(get_the_content(), 18);
+                            } ?> <a href="<?php the_permalink(); ?>" class="nu gray">Learn more</a></p>
+                    </div>
+                </div>
+            <?php }
+            ?>
+
+            <p class="t-center no-margin"><a href="<?php echo get_post_type_archive_link('event') ?>" class="btn btn--blue">View All Events</a></p>
+
         </div>
     </div>
     <div class="full-width-split__two">
         <div class="full-width-split__inner">
             <h2 class="headline headline--small-plus t-center">From Our Blogs</h2>
-
             <?php
             $homepagePosts = new WP_Query(array(
                 'posts_per_page' => 2
@@ -57,12 +78,19 @@
                     </a>
                     <div class="event-summary__content">
                         <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
-                        <p><?php echo wp_trim_words(get_the_content(), 18) ?><a href="<?php the_permalink(); ?>" class="nu gray">Read more</a></p>
+                        <p><?php if (has_excerpt()) {
+                                echo  get_the_excerpt();
+                            } else {
+                                echo wp_trim_words(get_the_content(), 18);
+                            } ?> <a href="<?php the_permalink(); ?>" class="nu gray">Read more</a></p>
                     </div>
                 </div>
             <?php }
             wp_reset_postdata();
             ?>
+
+
+
 
             <p class="t-center no-margin"><a href="<?php echo site_url('/blog'); ?>" class="btn btn--yellow">View All Blog Posts</a></p>
         </div>
@@ -72,7 +100,7 @@
 <div class="hero-slider">
     <div data-glide-el="track" class="glide__track">
         <div class="glide__slides">
-            <div class="hero-slider__slide" style="background-image: url(<?php echo get_theme_file_uri('/images/bus.jpg') ?>);">
+            <div class="hero-slider__slide" style="background-image: url(<?php echo get_theme_file_uri('/images/bus.jpg'); ?>);">
                 <div class="hero-slider__interior container">
                     <div class="hero-slider__overlay">
                         <h2 class="headline headline--medium t-center">Free Transportation</h2>
@@ -81,7 +109,7 @@
                     </div>
                 </div>
             </div>
-            <div class="hero-slider__slide" style="background-image: url(<?php echo get_theme_file_uri('/images/apples.jpg') ?>);">
+            <div class="hero-slider__slide" style="background-image: url(<?php echo get_theme_file_uri('/images/apples.jpg'); ?>);">
                 <div class="hero-slider__interior container">
                     <div class="hero-slider__overlay">
                         <h2 class="headline headline--medium t-center">An Apple a Day</h2>
@@ -90,7 +118,7 @@
                     </div>
                 </div>
             </div>
-            <div class="hero-slider__slide" style="background-image: url(<?php echo get_theme_file_uri('/images/bread.jpg') ?>);">
+            <div class="hero-slider__slide" style="background-image: url(<?php echo get_theme_file_uri('/images/bread.jpg'); ?>);">
                 <div class="hero-slider__interior container">
                     <div class="hero-slider__overlay">
                         <h2 class="headline headline--medium t-center">Free Food</h2>
@@ -100,10 +128,11 @@
                 </div>
             </div>
         </div>
-        <div class="slider__bullets glide__bullets" data-glide-el="controls[nav]"></div>
+        <div class="slider__bullets glide__bullets" data-glide-el="controls[nav]">
+        </div>
     </div>
 </div>
 
+<?php get_footer();
 
-
-<?php get_footer(); ?>
+?>
