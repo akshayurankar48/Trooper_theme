843aa0680bc74a494ab2f5305c52718c6d3b485d
evilieswithin <evilieswithin>
2024-01-01 17:31:43 +0530

    add new user roles

--- functions.php
+++ functions.php
@@ -13,56 +13,48 @@ function university_custom_rest()
 
 add_action('rest_api_init', 'university_custom_rest');
 
-add_action('rest_api_init', 'university_custom_rest');
-
 function pageBanner($args = NULL)
 {
-    // Check if $args is an array
-    if (!is_array($args)) {
-        $args = array(); // Initialize $args as an empty array if it's not set or not an array
+
+    if (!$args['title']) {
+        $args['title'] = get_the_title();
     }
 
-    // Check if 'title' key exists, otherwise get the default title
-    $args['title'] = isset($args['title']) ? $args['title'] : get_the_title();
+    if (!$args['subtitle']) {
+        $args['subtitle'] = get_field('page_banner_subtitle');
+    }
 
-    // Check if 'subtitle' key exists, otherwise get the default subtitle
-    $args['subtitle'] = isset($args['subtitle']) ? $args['subtitle'] : get_field('page_banner_subtitle');
-
-    // Check if 'photo' key exists
-    if (!isset($args['photo'])) {
-        // Check if 'page_banner_background_image' field exists
-        if (get_field('page_banner_background_image')) {
+    if (!$args['photo']) {
+        if (get_field('page_banner_background_image') and !is_archive() and !is_home()) {
             $args['photo'] = get_field('page_banner_background_image')['sizes']['pageBanner'];
         } else {
             $args['photo'] = get_theme_file_uri('/images/ocean.jpg');
         }
     }
+
 ?>
     <div class="page-banner">
-        <div class="page-banner__bg-image" style="background-image: url(<?php echo esc_url($args['photo']); ?>);"></div>
+        <div class="page-banner__bg-image" style="background-image: url(<?php echo $args['photo']; ?>);"></div>
         <div class="page-banner__content container container--narrow">
-            <h1 class="page-banner__title"><?php echo wp_kses_post($args['title']); ?></h1>
+            <h1 class="page-banner__title"><?php echo $args['title'] ?></h1>
             <div class="page-banner__intro">
-                <p><?php echo wp_kses_post($args['subtitle']); ?></p>
+                <p><?php echo $args['subtitle']; ?></p>
             </div>
         </div>
     </div>
-
-<?php
-}
+<?php }
 
 function university_files()
 {
+    wp_enqueue_script('googleMap', '//maps.googleapis.com/maps/api/js?key=AIzaSyDin3iGCdZ7RPomFLyb2yqFERhs55dmfTI', NULL, '1.0', true);
     wp_enqueue_script('main-university-js', get_theme_file_uri('/build/index.js'), array('jquery'), '1.0', true);
     wp_enqueue_style('custom-google-fonts', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
     wp_enqueue_style('font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
     wp_enqueue_style('university_main_styles', get_theme_file_uri('/build/style-index.css'));
     wp_enqueue_style('university_extra_styles', get_theme_file_uri('/build/index.css'));
 
-
     wp_localize_script('main-university-js', 'universityData', array(
-        'root_url' => get_site_url(),
-        'sky' => 'blue'
+        'root_url' => get_site_url()
     ));
 }
 
@@ -73,7 +65,7 @@ function university_features()
     add_theme_support('title-tag');
     add_theme_support('post-thumbnails');
     add_image_size('professorLandscape', 400, 260, true);
-    add_image_size('professorPotrait', 480, 650, true);
+    add_image_size('professorPortrait', 480, 650, true);
     add_image_size('pageBanner', 1500, 350, true);
 }
 
@@ -81,19 +73,22 @@ add_action('after_setup_theme', 'university_features');
 
 function university_adjust_queries($query)
 {
-    if (!is_admin() and is_post_type_archive('program') and is_main_query()) {
+    if (!is_admin() and is_post_type_archive('campus') and $query->is_main_query()) {
+        $query->set('posts_per_page', -1);
+    }
+
+    if (!is_admin() and is_post_type_archive('program') and $query->is_main_query()) {
         $query->set('orderby', 'title');
         $query->set('order', 'ASC');
         $query->set('posts_per_page', -1);
     }
-
 
     if (!is_admin() and is_post_type_archive('event') and $query->is_main_query()) {
         $today = date('Ymd');
         $query->set('meta_key', 'event_date');
         $query->set('orderby', 'meta_value_num');
         $query->set('order', 'ASC');
-        $query->set('meta_query',  array(
+        $query->set('meta_query', array(
             array(
                 'key' => 'event_date',
                 'compare' => '>=',
@@ -106,27 +101,59 @@ function university_adjust_queries($query)
 
 add_action('pre_get_posts', 'university_adjust_queries');
 
-
 function universityMapKey($api)
 {
-    $api['key'] = 'AIzaSyB7q295RD6nhQcE5VFuvdbAPmV20_NLhSw';
+    $api['key'] = 'yourKeyGoesHere';
     return $api;
 }
 
 add_filter('acf/fields/google_map/api', 'universityMapKey');
 
+// Redirect subscriber accounts out of admin and onto homepage
+add_action('admin_init', 'redirectSubsToFrontend');
 
-// $words = array('We', 'are', 'debugging!');
-// foreach ($words as $word) {
-//     echo $word;
-// }
+function redirectSubsToFrontend()
+{
+    $ourCurrentUser = wp_get_current_user();
 
-// for ($i = 0; $i < 100; $i++) {
-//     echo $i, "\n";
-// }
+    if (count($ourCurrentUser->roles) == 1 and $ourCurrentUser->roles[0] == 'subscriber') {
+        wp_redirect(site_url('/'));
+        exit;
+    }
+}
 
-// $values = ['Xdebug', 'is', 'such', 'a', 'useful', 'tool'];
+add_action('wp_loaded', 'noSubsAdminBar');
 
-// $values = array_map(fn ($value) => strtoupper($value), $values);
+function noSubsAdminBar()
+{
+    $ourCurrentUser = wp_get_current_user();
 
-// echo join(' ', $values);
+    if (count($ourCurrentUser->roles) == 1 and $ourCurrentUser->roles[0] == 'subscriber') {
+        show_admin_bar(false);
+    }
+}
+
+// Customize Login Screen
+add_filter('login_headerurl', 'ourHeaderUrl');
+
+function ourHeaderUrl()
+{
+    return esc_url(site_url('/'));
+}
+
+add_action('login_enqueue_scripts', 'ourLoginCSS');
+
+function ourLoginCSS()
+{
+    wp_enqueue_style('custom-google-fonts', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
+    wp_enqueue_style('font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
+    wp_enqueue_style('university_main_styles', get_theme_file_uri('/build/style-index.css'));
+    wp_enqueue_style('university_extra_styles', get_theme_file_uri('/build/index.css'));
+}
+
+add_filter('login_headertitle', 'ourLoginTitle');
+
+function ourLoginTitle()
+{
+    return get_bloginfo('name');
+}
